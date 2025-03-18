dc35fb7d2179e1707ca9692de62428da273e205e
Marc Hanson <marc.hanson.github@gmail.com>
2025-03-17 23:46:24 -0400

    SHARD-2019 - Prettier tools-shardus-cli

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,10 @@ module.exports = {
   testMatch: ['**/test/**/*.test.(js|ts)', '**/?(*.)+(spec|test).(js|ts)'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'index.js',
-    'bin/**/*.js',
-    '!**/node_modules/**',
-    '!**/build/**',
-  ],
+  collectCoverageFrom: ['index.js', 'bin/**/*.js', '!**/node_modules/**', '!**/build/**'],
   coverageReporters: ['text', 'lcov', 'json-summary'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-}; 
+}
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -2,58 +2,58 @@
 
 // Instead of mocking the modules, we'll test the structure of the exported object
 describe('Shardus CLI Module', () => {
-  let shardusModule;
-  
+  let shardusModule
+
   beforeEach(() => {
-    jest.resetModules();
-    shardusModule = require('../../index');
-  });
+    jest.resetModules()
+    shardusModule = require('../../index')
+  })
 
   test('should export register object with network and debug properties', () => {
-    expect(shardusModule).toHaveProperty('register');
-    expect(shardusModule.register).toHaveProperty('network');
-    expect(shardusModule.register).toHaveProperty('debug');
-  });
+    expect(shardusModule).toHaveProperty('register')
+    expect(shardusModule.register).toHaveProperty('network')
+    expect(shardusModule.register).toHaveProperty('debug')
+  })
 
   test('should export lib object with network and debug properties', () => {
-    expect(shardusModule).toHaveProperty('lib');
-    expect(shardusModule.lib).toHaveProperty('network');
-    expect(shardusModule.lib).toHaveProperty('debug');
-  });
+    expect(shardusModule).toHaveProperty('lib')
+    expect(shardusModule.lib).toHaveProperty('network')
+    expect(shardusModule.lib).toHaveProperty('debug')
+  })
 
   test('register.network should contain network module register functions', () => {
     // Just check that it has functions, not specific mock functions
-    const networkFunctions = Object.values(shardusModule.register.network);
-    expect(networkFunctions.length).toBeGreaterThan(0);
-    networkFunctions.forEach(fn => {
-      expect(typeof fn).toBe('function');
-    });
-  });
+    const networkFunctions = Object.values(shardusModule.register.network)
+    expect(networkFunctions.length).toBeGreaterThan(0)
+    networkFunctions.forEach((fn) => {
+      expect(typeof fn).toBe('function')
+    })
+  })
 
   test('register.debug should contain debug module register functions', () => {
     // Just check that it has functions, not specific mock functions
-    const debugFunctions = Object.values(shardusModule.register.debug);
-    expect(debugFunctions.length).toBeGreaterThan(0);
-    debugFunctions.forEach(fn => {
-      expect(typeof fn).toBe('function');
-    });
-  });
+    const debugFunctions = Object.values(shardusModule.register.debug)
+    expect(debugFunctions.length).toBeGreaterThan(0)
+    debugFunctions.forEach((fn) => {
+      expect(typeof fn).toBe('function')
+    })
+  })
 
   test('lib.network should contain network module lib functions', () => {
     // Just check that it has functions, not specific mock functions
-    const networkLibFunctions = Object.values(shardusModule.lib.network);
-    expect(networkLibFunctions.length).toBeGreaterThan(0);
-    networkLibFunctions.forEach(fn => {
-      expect(typeof fn).toBe('function');
-    });
-  });
+    const networkLibFunctions = Object.values(shardusModule.lib.network)
+    expect(networkLibFunctions.length).toBeGreaterThan(0)
+    networkLibFunctions.forEach((fn) => {
+      expect(typeof fn).toBe('function')
+    })
+  })
 
   test('lib.debug should contain debug module lib functions', () => {
     // Just check that it has functions, not specific mock functions
-    const debugLibFunctions = Object.values(shardusModule.lib.debug);
-    expect(debugLibFunctions.length).toBeGreaterThan(0);
-    debugLibFunctions.forEach(fn => {
-      expect(typeof fn).toBe('function');
-    });
-  });
-}); 
+    const debugLibFunctions = Object.values(shardusModule.lib.debug)
+    expect(debugLibFunctions.length).toBeGreaterThan(0)
+    debugLibFunctions.forEach((fn) => {
+      expect(typeof fn).toBe('function')
+    })
+  })
+})
